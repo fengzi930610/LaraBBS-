356c9053f9d8bef21a0b5370d5a85628da536648
fengzi930610 <1057780049@qq.com>
2020-03-18 19:25:51 +0800

    基础页面结构

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get("/", "PagesController@root")->name("root");
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
